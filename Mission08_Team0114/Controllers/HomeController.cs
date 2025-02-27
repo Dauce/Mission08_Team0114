74676bedf7224e01eefe1dd0e723401a33535e94
Hannah Gouff <hannahgouff@gmail.com>
2025-02-27 11:30:23 -0700

    fixing the errors!!

--- Mission08_Team0114/Controllers/HomeController.cs
+++ Mission08_Team0114/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Mission08_Team0114.Models;
 
 namespace Mission08_Team0114.Controllers;
@@ -15,7 +16,11 @@ public class HomeController : Controller
 
     public IActionResult Quadrants()
     {
-        return View();
+        var tasks = _context.Tasks
+            .Include(x=>x.categoryName)
+            .ToList();
+        
+        return View(tasks);
     }
 
     public IActionResult Privacy()
