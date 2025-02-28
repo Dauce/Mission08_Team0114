c577d51a19e5a78840c9d1b40120d3eb00708022
Hannah Gouff <hannahgouff@gmail.com>
2025-02-28 12:08:55 -0700

    Fixed the save changes button, added Edit and Delete routes. Everything should be working!

--- Mission08_Team0114/Controllers/HomeController.cs
+++ Mission08_Team0114/Controllers/HomeController.cs
@@ -18,10 +18,10 @@ public class HomeController : Controller
     public IActionResult Quadrants()
     {
         var tasks = _context.Tasks
-            .Include(x=>x.Categories)
+            .Include(x => x.Categories)
             .Where(x => x.completed == 0)
             .ToList();
-        
+
         return View(tasks);
     }
 
@@ -43,22 +43,33 @@ public class HomeController : Controller
 
             return RedirectToAction("Quadrants", response);
         }
-        else // Invalid data
+        else
         {
-            ViewBag.Categories = _context.Categories
-            .ToList();
+            ViewBag.Categories = _context.Categories.ToList();
 
-            return View(response);
+            return View("AddTask", new Mission08_Team0114.Models.Task());
         }
     }
+
+    [HttpGet]
 
     public IActionResult Delete(int taskId)
     {
         var taskToDelete = _context.Tasks
-                .Single(x => x.taskId == taskId);
+            .Single(x => x.taskId == taskId);
 
         return View(taskToDelete);
     }
+
+    [HttpPost]
+    public IActionResult Delete(Mission08_Team0114.Models.Task task)
+    {
+        _context.Tasks.Remove(task);
+        _context.SaveChanges();
+        return RedirectToAction("Quadrants", task);
+    }
+
+
 
     public IActionResult Privacy()
     {
@@ -69,5 +80,48 @@ public class HomeController : Controller
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+
+
+
+
+
+
+    [HttpGet]
+
+    public IActionResult EditTask(int taskId)
+    {
+        Mission08_Team0114.Models.Task task = _context.Tasks
+            .Where(x => x.taskId == taskId)
+            .SingleOrDefault();
+        if (task == null)
+        {
+            return NotFound(); // Prevents a null reference error if the task doesn't exist
+        }
+
+        ViewBag.Categories = _context.Categories
+            .OrderBy(x =>x.categoryId)
+            .ToList();
+        return View("AddTask", task);
+    }
+
+    [HttpPost]
+    public IActionResult EditTask(Mission08_Team0114.Models.Task response)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Tasks.Update(response); // Add record to the database
+            _context.SaveChanges();
+
+            return RedirectToAction("Quadrants", response);
+        }
+        else // Invalid data
+        {
+            ViewBag.Categories = _context.Categories
+                .ToList();
+
+            return View("AddTask", response);
+        }
     }
 }
